20b2ee279339e339c240d62675bb4f48de62c816
SergiiBulan <rotyara@gmail.com>
2022-07-22 13:18:13 +0300

    new

---
+++ homworckJs/hom1/non.js
@@ -0,0 +1,103 @@
+let a;
+a='hello';
+/*console.log(a);
+document.write(a);*/
+/*alert(a);*/
+
+let b;
+b = 'owu';
+/*console.log(b);
+document.write(b);
+alert(b);*/
+
+let c;
+c='com';
+/*
+console.log(c);
+document.write(c)
+alert(c);
+*/
+
+let d;
+d='ua';
+/*/!*
+console.log(d);
+document.write(d);
+*!/
+alert(d);*/
+
+let e;
+e=1;
+/*console.log(e);
+document.write(e);
+alert(e);*/
+
+let f;
+f=10;
+/*console.log(f);
+document.write(f);
+alert(f);*/
+
+let g;
+g=-999;
+/*console.log(g);
+document.write(g);
+alert(g);*/
+
+let bob;
+bob=123;
+/*console.log(bob);
+document.write(bob);
+alert(bob);*/
+
+let kok;
+kok=3.14;
+/*console.log(kok);
+document.write(kok);
+alert(kok);*/
+
+let lol;
+lol=2.7;
+/*console.log(lol);
+document.write(lol);
+alert(lol);*/
+
+let wow;
+wow=16;
+/*console.log(wow);
+document.write(wow);
+alert(wow);*/
+
+let kill;
+kill=true;
+/*console.log(kill);
+document.write(kill);
+alert(kill);*/
+
+let rio;
+rio=false;
+/*console.log(rio);
+document.write(rio);
+alert(rio);*/
+
+console.log(a,b,c,d,e,f,g,bob,kok,lol,wow,kill,rio);
+document.write(a,b,c,d,e,f,g,bob,kok,lol,wow,kill,rio);
+
+/*console.log(typeof f);
+console.log(typeof rio);
+console.log(typeof a);*/
+
+/*let res=a*d;
+console.log(res);*/
+
+let first;
+first='Bulan';
+let middle;
+middle='Serhii';
+let last;
+last='Andriyovich';
+let person1=first+' '+middle+' '+last;
+console.log(person1);
+/*
+let person2=`${first} ${middle} ${last}`;
+console.log(person2);*/
