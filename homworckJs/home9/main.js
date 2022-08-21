c36a6dd3e5aae5ff99dbf326d6a177c79394cce3
SergoBulan <108830039+SergoBulan@users.noreply.github.com>
2022-08-21 19:40:56 +0300

    new

--- homworckJs/home9/main.js
+++ homworckJs/home9/main.js
@@ -1,62 +1,66 @@
 // Все робити за допомоги js.
 
-
+// ?????????????????????????????????????????????????????????????????
 // - створити блок,
 //     - додати йому класи wrap, collapse, alpha, beta
 // - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
 // - додати цей блок в body.
 // - клонувати його повністю, та додати клон в body.
 
-// let sides = [
-//     {
-//         name: 'Свино-собаки',
-//         status: 'terrorist',
-//         solders: 1500000,
-//         flag: 'https://24tv.ua/resources/photos/news/1200x1200_DIR/202203/1924214.jpg',
-//     },
-//     {
-//         name: 'Ukraine',
-//         status: 'heroic-defender',
-//         solders: 250000,
-//         flag: 'https://gdb.rferl.org/24293B5E-DC8F-4AA0-9845-CA844C0ECD49_w1080_h608.jpg',
-//     },
-//     {
-//         name: 'Бульбаші',
-//         status: 'terrorists-assistant',
-//         solders: 200000,
-//         flag: 'https://zname.com.ua/watermark/watermark.php?image=https://zname.com.ua/image/cache/catalog/content/Flagi-mira/Belarus_2x3-800x533.jpg',
-//     }
-// ];
-//
-// for (const items of sides) {
-//     let divInfo = document.createElement('div');
-//     divInfo.classList.add('wrap');
-//     wrap.style.width = '200px';
-//
-//     let h2 = document.createElement('h2');
-//     h2.classList.add('alpha');
-//     h2.alpha.innerText = items.name;
-//     h2.style.beckground = blue;
-//     h2.style.length = '200px';
-//
-//     let p = document.createElement('p');
-//     p.classList.add('beta')
-//     p.innerText = items.solders + ' ' + items.status;
-//
-//     let print = document.createElement('img');
-//     print.classList.add('collapse');
-//     print.src = items.flag;
-//     collapse.style.width = '100%';
-//
-//     divInfo.wrap.appendChild(alpha);
-//     divInfo.wrap.append(beta);
-//     divInfo.wrap.append(collapse);
-//
-//     document.body.appendChild(divInfo);
-// };
-// wrap.appendChild(divInfo.cloneNode(true));
+let sides = [
+    {
+        name: 'Свино-собаки',
+        status: 'terrorist',
+        solders: 1500000,
+        flag: 'https://24tv.ua/resources/photos/news/1200x1200_DIR/202203/1924214.jpg',
+    },
+    {
+        name: 'Ukraine',
+        status: 'heroic-defender',
+        solders: 250000,
+        flag: 'https://gdb.rferl.org/24293B5E-DC8F-4AA0-9845-CA844C0ECD49_w1080_h608.jpg',
+    },
+    {
+        name: 'Бульбаші',
+        status: 'terrorists-assistant',
+        solders: 200000,
+        flag: 'https://zname.com.ua/watermark/watermark.php?image=https://zname.com.ua/image/cache/catalog/content/Flagi-mira/Belarus_2x3-800x533.jpg',
+    }
+];
+
+for (const items of sides) {
+    if (items.length !== 0) {
+        let divInfo = document.createElement('div');
+        divInfo.classList.add('wrap');
+        document.body.appendChild(divInfo);
+
+        let h2 = document.createElement('h2');
+        h2.classList.add('alpha');
+        h2.alpha.innerText = items.name;
+        h2.style.beckground = blue;
+
+
+        let p = document.createElement('p');
+        p.classList.add('beta')
+        p.innerText = items.solders + ' ' + items.status;
+
+        let print = document.createElement('img');
+        print.classList.add('collapse');
+        print.src = items.flag;
+    }
+
+    divInfo.wrap.appendChild(alpha);
+    divInfo.wrap.append(beta);
+    divInfo.wrap.append(collapse);
+
+
+};
+wrap.appendChild(divInfo.cloneNode(true));
 
 //чому спочатку все зпрацювало а потім після виконання classList.add помилки бити почало? навіть при приберанні цієї дії
+
+// ___________________________________________________________________________________________________________________
+
 
 
 // - Є масив:
@@ -80,7 +84,7 @@
 //     }
 // }
 // creatBlock(howArray);
-//
+
 //////////////////////////////////////
 // for (const item of array) {
 //     let liList = document.createElement('li');
@@ -112,10 +116,12 @@
 //     {title: 'FullStack', monthDuration: 7},
 //     {title: 'Frontend', monthDuration: 4}
 // ];
+
+
 // let div = document.createElement('div');
 // div.classList.add('target');
 // document.body.appendChild(div);
-//
+
 // function creatBlock (array) {
 //     for (const item of array){
 //         if (item.length !== 0){
@@ -129,7 +135,10 @@
 // creatBlock(coursesAndDurationArray);
 
 
-//
+
+
+
+
 // - Є масив
 // let coursesAndDurationArray = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
@@ -153,14 +162,19 @@
 //     {title: 'FullStack', monthDuration: 7},
 //     {title: 'Frontend', monthDuration: 4}
 // ];
-// let div = document.createElement('div');
-// div.classList.add('target');
-// document.body.appendChild(div);
+//
+// let divFirst = document.createElement('div');
+// divFirst.classList.add('alpha');
+// document.body.appendChild(divFirst);
 //
 //
 // function creatBlock (array) {
 //     for (const item of array){
 //         if (item.length !== 0){
+//             let div = document.createElement('div');
+//             div.classList.add('beta');
+//             divFirst.appendChild(div);
+//
 //             let h2 = document.createElement('h2');
 //             h2.classList.add('heading');
 //             h2.innerText = item.title;
@@ -175,7 +189,192 @@
 //     }
 // }
 // creatBlock(coursesAndDurationArray);
+
+
+
+
+
+// ============================================================================================================
+
+// - є масив
+// let simpsons = [
+//     {
+//         name: 'Bart',
+//         surname: 'Simpson',
+//         age: 10,
+//         info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+//     },
+//     {
+//         name: 'Homer',
+//         surname: 'Simpson',
+//         age: 40,
+//         info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+//         photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+//     },
+//     {
+//         name: 'Marge',
+//         surname: 'Simpson',
+//         age: 38,
+//         info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+//     },
+//     {
+//         name: 'Lisa',
+//         surname: 'Simpson',
+//         age: 9,
+//         info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+//     },
+//     {
+//         name: 'Maggie',
+//         surname: 'Simpson',
+//         age: 1,
+//         info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+//     },
+// ];
 //
+// let div = document.createElement('div');
+// div.classList.add('member');
+//
+//
+// for (const item of simpsons){
+//     if (item.length !== 0) {
+//         let h2 = document.createElement('h2');
+//             h2.classList.add('first');
+//             h2.innerText = item.name + ' ' + item.surname;
+//             div.appendChild(h2);
+//
+//             let p = document.createElement('p');
+//             p.classList.add('second');
+//             p.innerText = item.info + 'age' + ' ' + item.age;
+//             div.appendChild(p);
+//
+//             let imgAll = document.createElement('img');
+//             imgAll.classList.add('pictur');
+//             imgAll.src = item.photo;
+//             div.appendChild(imgAll);
+//     }
+// };
+// document.body.appendChild(div);
+
+// Проітерувати його, створиши для кожного елементу масиву <div class='member'>.
+// - взяти попередній масив з сімпсонами.
+//     Проітерувати його, створиши для кожного елементу масиву <div class='member'>.
+//     Для кожної властивості елементу створити окремий блок, та помістити його у div.member
+
+
+
+
+
+
+
+// - Є масив
+// let coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'git',
+//             'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html',
+//         'css',
+//         'js',
+//         'mysql',
+//         'mongodb',
+//         'react',
+//         'angular',
+//         'aws',
+//         'docker',
+//         'git',
+//         'sass']
+//     }
+// ];
+
+
+// Створити для кожного елементу масиву свій блок, блок розділити блоками,
+// в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+// Приклад структири знаходиться у файлі example.png
+
+
+
+
 
 
 
